5598bd37e8eecdd86f8244ebb2a518489159ceeb
Małgorzata Kurach <96025745+mkurach@users.noreply.github.com>
2022-11-08 18:13:02 +0100

    added line, new range, moved legend

--- compCorrFunc.c
+++ compCorrFunc.c
@@ -37,7 +37,7 @@ void makePaveText(TVirtualPad* can, TString text, double x1, double y1, double x
     description->Draw("same");
 }
 
-void multiplePlot(TH1D *histTab[], TCanvas* can, TString* entry, size_t n) {
+void multiplePlot(TH1D *histTab[], TCanvas* can, TString* entry, size_t n, bool up) {
 	
     Double_t max = 0;
     Double_t min = 0;
@@ -54,7 +54,11 @@ void multiplePlot(TH1D *histTab[], TCanvas* can, TString* entry, size_t n) {
     histTab[0]->SetTitle("");
     histTab[0]->SetStats(kFALSE);
 
-    TLegend* legend	= new TLegend(0.63,0.2,0.93,0.5, "",	"NDC");
+    TLegend* legend;
+    if (up)
+        legend	= new TLegend(0.7,0.6,0.93,0.8, "",	"NDC");
+    else
+        legend	= new TLegend(0.7,0.2,0.93,0.4, "",	"NDC");
     legend->SetFillColor(0);
     legend->SetFillStyle(0);
     legend->SetTextSize(0.03);
@@ -97,6 +101,18 @@ void multiplePlot(TH1D *histTab[], TCanvas* can, TString* entry, size_t n) {
 
 
 
+
+
+}
+
+void drawLine(TH1D* hist, TCanvas* can) {
+    TLine* line = new TLine(hist->GetXaxis()->GetBinLowEdge(1),1,hist->GetXaxis()->GetBinLowEdge(hist->FindBin(0.15)),1);
+    line->SetLineWidth(2);
+    line->SetLineStyle(2);
+    line->SetLineColor(kBlack);
+    can->cd();
+    line->Draw();
+
 }
 
 
@@ -107,6 +123,7 @@ void fixedPairs() {
     TCanvas *can[_N_CASES_][_N_PAIRS_][_N_HIST_-1];
     TString entries[7] = {"#epsilon = 0.0","#epsilon = 0.1","#epsilon = 0.2","#epsilon = 0.3","#epsilon = 0.4","#epsilon = 0.5","#epsilon = 0.6"};
 
+    //READING
     TFile *fileOut[_N_PAIRS_];
     for (int i = 0; i < _N_PAIRS_; i ++)
         fileOut[i] = new TFile(Form("./output/outputComp/fixedPairs%s.root",pairs[i].Data()), "RECREATE");
@@ -121,19 +138,58 @@ void fixedPairs() {
                    hist[i][j][1][k] = (TH1D*) fileIn[i][j]->Get(Form("%sE%d%sCorrFuncQsc",cases[i].Data(),k,pairs[j].Data()));
             }
 
+        }
+
+    }
+
+    //DRAWING
+    for(int i = 0; i < _N_CASES_; i ++) {
+        for(int j = 0; j < _N_PAIRS_; j++) {
+            for (int k = 0; k < _N_EPSILON_; k++) {
+
+                if (j == 0) {
+                    hist[i][j][0][k]->Rebin(5);
+                    hist[i][j][0][k]->Scale(0.2);
+                    hist[i][j][1][k]->Rebin(5);
+                    hist[i][j][1][k]->Scale(0.2);
+                    
+                    hist[i][j][1][k]->GetXaxis()->SetRangeUser(0.0,0.15);
+                    
+                }
+                hist[i][j][0][k]->GetXaxis()->SetRangeUser(0.0,0.15);
+            }
+
             
             can[i][j][0] = new TCanvas(Form("%s%sEpsilonComp",cases[i].Data(),pairs[j].Data()),Form("%s%sEpsilonComp",cases[i].Data(),pairs[j].Data()),800,800);
-            multiplePlot(hist[i][j][0],can[i][j][0],entries,7);
-            makePaveText(can[i][j][0],casesNice[i].Data(),0.6,0.7,0.8,0.75);
-            makePaveText(can[i][j][0],pairsNice[i].Data(),0.6,0.75,0.8,0.8);
+            if( j == 0) {
+                multiplePlot(hist[i][j][0],can[i][j][0],entries,7,false);
+                makePaveText(can[i][j][0],casesNice[i].Data(),0.55,0.3,0.7,0.35);
+                makePaveText(can[i][j][0],pairsNice[j].Data(),0.55,0.35,0.7,0.4);
+            }
+            else {
+                multiplePlot(hist[i][j][0],can[i][j][0],entries,7,true);
+                makePaveText(can[i][j][0],casesNice[i].Data(),0.55,0.7,0.7,0.75);
+                makePaveText(can[i][j][0],pairsNice[j].Data(),0.55,0.75,0.7,0.8);
+            }
+
+            drawLine(hist[i][j][0][0],can[i][j][0]);
             fileOut[j]->cd();
             can[i][j][0]->Write();
 
             if (j == 0) {
                 can[i][j][1] = new TCanvas(Form("%s%sEpsilonCompQsc",cases[i].Data(),pairs[j].Data()),Form("%s%sEpsilonComp",cases[i].Data(),pairs[j].Data()),800,800);
-                multiplePlot(hist[i][j][1],can[i][j][1],entries,7);
-                makePaveText(can[i][j][1],casesNice[i].Data(),0.6,0.7,0.8,0.75);
-                makePaveText(can[i][j][1],pairsNice[i].Data(),0.6,0.75,0.8,0.8);
+                if(i == 0) {
+                    multiplePlot(hist[i][j][1],can[i][j][1],entries,7,false);
+                    makePaveText(can[i][j][1],casesNice[i].Data(),0.55,0.3,0.7,0.35);
+                    makePaveText(can[i][j][1],pairsNice[j].Data(),0.55,0.35,0.7,0.4);
+                }
+                else {
+                    multiplePlot(hist[i][j][1],can[i][j][1],entries,7,true);
+                    makePaveText(can[i][j][1],casesNice[i].Data(),0.55,0.7,0.7,0.75);
+                    makePaveText(can[i][j][1],pairsNice[j].Data(),0.55,0.75,0.7,0.8);
+                }
+
+                drawLine(hist[i][j][0][1],can[i][j][1]);
                 fileOut[j]->cd();
                 can[i][j][1]->Write();
             }
@@ -155,5 +211,7 @@ void fixedPairs() {
 }
 
 void compCorrFunc() {
+    gROOT -> SetBatch(kTRUE);
     fixedPairs();
+    gROOT -> SetBatch(kFALSE);
 }

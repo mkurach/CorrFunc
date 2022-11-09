4ed7223d4fc8db1d0090a63719cf767ba9b9c757
Małgorzata Kurach <96025745+mkurach@users.noreply.github.com>
2022-11-09 16:55:10 +0100

    performing fits

--- makeCorrFunc.c
+++ makeCorrFunc.c
@@ -7,93 +7,38 @@
 
 
 TString cases[_N_CASES_] = {"caseA","caseB","caseC"};
-TString hubb[_N_CASES_] = {"H100","H225","H165"};
-TString delt[_N_CASES_] = {"D2","D4","D4"};
+//TString hubb[_N_CASES_] = {"H100","H225","H165"};
+//TString delt[_N_CASES_] = {"D2","D4","D4"};
 TString pairs[_N_PAIRS_] = {"pp","pipi","piMpiM"};
-TString hists[_N_HIST_] = {"num1d","den1d","num1dqsc"};
+//TString hists[_N_HIST_] = {"num1d","den1d","num1dqsc"};
 
-void readLustre(){
-    TH1D* hist[_N_CASES_][_N_EPSILON_][_N_PAIRS_][_N_HIST_];
-    TFile* file[_N_CASES_][_N_EPSILON_][_N_PAIRS_];
-    TFile *fileOut[_N_CASES_];
-    
-    for(int i = 0; i < _N_CASES_; i++) {
-        fileOut[i] = new TFile(Form("./output/%s.root",cases[i].Data()), "RECREATE");
-        for(int j = 0; j < _N_EPSILON_; j++) {
-            for(int k = 0; k < _N_PAIRS_; k++) {
-                file[i][j][k] = new TFile(Form("../lustre/hades/user/mkurach/inz/%s/%sE%d%s/femto%s19a.root",cases[i].Data(),hubb[i].Data(),j,delt[i].Data(),pairs[k].Data()));
-                for (int l = 0; l < _N_HIST_; l++) {
-                    if((l == 2 && k == 1) || (l == 2 && k == 2)) //pairs without qsc
-                        continue;
-                    else {
-                        hist[i][j][k][l] = (TH1D*) file[i][j][k]->Get(hists[l].Data());
-                        hist[i][j][k][l] = (TH1D*)hist[i][j][k][l]->Clone(Form("%sE%d%s%s",cases[i].Data(),j,pairs[k].Data(),hists[l].Data()));
-                        fileOut[i]->cd();
-                        hist[i][j][k][l]->Write();
-                    }
-                }
-            }
-        }
-        fileOut[i]->Save();
-        fileOut[i]->Close();
-    }
 
+
+Double_t fitFunction(Double_t *x, Double_t *par) { // 0 - lambda, 1 - R
+    if(x[0] >= 0)
+        return 1+par[0]*TMath::Exp(-par[1]*par[1]*x[0]*x[0]);
+    else    
+        return 0;
 }
 
 void makeCorrFunc() {
-    TH1D* hist[_N_CASES_][_N_EPSILON_][_N_PAIRS_][_N_HIST_-1];
-    TFile *fileIn[_N_CASES_];
-    TFile *fileOut[_N_CASES_];
-
-    for(int i = 0; i < _N_CASES_; i++) {
-        fileIn[i] = new TFile(Form("./output/%s.root",cases[i].Data()));
-        fileOut[i] = new TFile(Form("./output/%sCorrFun.root",cases[i].Data()), "RECREATE");
-        for(int j = 0; j < _N_EPSILON_; j++) {
-            for(int k = 0; k < _N_PAIRS_; k++) { 
-
-                hist[i][j][k][0] = (TH1D*) fileIn[i]->Get(Form("%sE%d%snum1d",cases[i].Data(),j,pairs[k].Data()));
-                hist[i][j][k][0] = (TH1D*) hist[i][j][k][0]->Clone(Form("%sE%d%sCorrFunc",cases[i].Data(),j,pairs[k].Data()));
-                hist[i][j][k][0]->Divide((TH1D*) fileIn[i]->Get(Form("%sE%d%sden1d",cases[i].Data(),j,pairs[k].Data())));
-                fileOut[i]->cd();
-                hist[i][j][k][0]->SetTitle("");
-                hist[i][j][k][0]->Write();
-                if(k == 0) { //pairs with qsc
-                    hist[i][j][k][1] = (TH1D*) fileIn[i]->Get(Form("%sE%d%snum1dqsc",cases[i].Data(),j,pairs[k].Data()));
-                    hist[i][j][k][1] = (TH1D*) hist[i][j][k][1]->Clone(Form("%sE%d%sCorrFuncQsc",cases[i].Data(),j,pairs[k].Data()));
-                    hist[i][j][k][1]->Divide((TH1D*) fileIn[i]->Get(Form("%sE%d%sden1d",cases[i].Data(),j,pairs[k].Data())));
-                    fileOut[i]->cd();
-                    hist[i][j][k][1]->SetTitle("");
-                    hist[i][j][k][1]->Write();
-                }
-
-                
-            }
-        }
-        fileIn[i]->Close();
-        fileOut[i]->Save();
-        fileOut[i]->Close();
-    }
-
-
-
-}
-
-void makeCorrFuncPairs() {
     TH1D* hist[_N_CASES_][_N_PAIRS_][_N_EPSILON_][_N_HIST_-1];
     TFile *fileIn[_N_CASES_];
     TFile *fileOut[_N_CASES_][_N_PAIRS_];
 
+    //READING
+
     for (int i = 0; i < _N_CASES_; i++) {
-        fileIn[i] = new TFile(Form("./output/%s.root",cases[i].Data()));
+        fileIn[i] = new TFile(Form("./outputLustre/%s.root",cases[i].Data()));
         for (int j = 0; j < _N_PAIRS_; j++) {
-            fileOut[i][j] = new TFile(Form("./output/%s%s.root",cases[i].Data(),pairs[j].Data()), "RECREATE");
+            fileOut[i][j] = new TFile(Form("./outputCorrFunc/%s%s.root",cases[i].Data(),pairs[j].Data()), "RECREATE");
             for(int k = 0; k < _N_EPSILON_; k++) {
                 hist[i][j][k][0] = (TH1D*) fileIn[i]->Get(Form("%sE%d%snum1d",cases[i].Data(),k,pairs[j].Data()));
                 hist[i][j][k][0] = (TH1D*) hist[i][j][k][0]->Clone(Form("%sE%d%sCorrFunc",cases[i].Data(),k,pairs[j].Data()));
                 hist[i][j][k][0]->Divide((TH1D*) fileIn[i]->Get(Form("%sE%d%sden1d",cases[i].Data(),k,pairs[j].Data())));
-                fileOut[i][j]->cd();
+                //fileOut[i][j]->cd();
                 hist[i][j][k][0]->SetTitle("");
-                hist[i][j][k][0]->Write();
+                //hist[i][j][k][0]->Write();
                 if(j == 0) { //pairs with qsc
                     hist[i][j][k][1] = (TH1D*) fileIn[i]->Get(Form("%sE%d%snum1dqsc",cases[i].Data(),k,pairs[j].Data()));
                     hist[i][j][k][1] = (TH1D*) hist[i][j][k][1]->Clone(Form("%sE%d%sCorrFuncQsc",cases[i].Data(),k,pairs[j].Data()));
@@ -104,21 +49,79 @@ void makeCorrFuncPairs() {
                 }
 
             }
-            fileOut[i][j]->Close();
-            fileOut[i][j]->Save();
+            //fileOut[i][j]->Close();
+            //fileOut[i][j]->Save();
 
 
         }
-        fileIn[i]->Close();
+        //fileIn[i]->Close();
     }
 
+
+    //FUNCTION
+    Int_t params = 2;
+    TF1 *fun = new TF1("fun",fitFunction,0,0.5,params);  //0 - lambda, 1 - R
+    fun->SetParameter(0,0.5);
+    fun->SetParameter(1,10);
+
+    //FITTING
+    Double_t lambdy[_N_CASES_][_N_PAIRS_][_N_EPSILON_];
+    Double_t r[_N_CASES_][_N_PAIRS_][_N_EPSILON_];
+    Double_t lambdyErr[_N_CASES_][_N_PAIRS_][_N_EPSILON_];
+    Double_t rErr[_N_CASES_][_N_PAIRS_][_N_EPSILON_];
+
+    Double_t xValues[_N_EPSILON_] = {0.0,0.1,0.2,0.3,0.4,0.5,0.6};
+    Double_t xErr[_N_EPSILON_] = {0};
+    Double_t minFit = 0;
+    Double_t maxFit = 0.5;
+
+
+    for(int i = 0; i < _N_CASES_; i ++) {
+        for(int j = 0; j < _N_PAIRS_; j++) {
+            for(int k = 0; k < _N_EPSILON_; k++) {
+                hist[i][j][k][0]->Fit("fun","","",minFit,maxFit);
+                fileOut[i][j]->cd();
+                hist[i][j][k][0]->Write();
+
+                lambdy[i][j][k] = fun->GetParameter(0);
+                lambdyErr[i][j][k] = fun->GetParError(0);
+                r[i][j][k] = fun->GetParameter(1);
+                r[i][j][k] = fun->GetParError(1);
+            }
+            fileOut[i][j]->Close();
+            fileOut[i][j]->Save();
+
+        }
+    }
+
+    //COMPARING FITTING RESULTS
+    TGraphErrors *grLambda[_N_CASES_][_N_PAIRS_];
+    TGraphErrors *grR[_N_CASES_][_N_PAIRS_];
+
+
+    TFile *fileOut2 = new TFile("./outputCorrFunc/fitResults.root", "RECREATE");
+    for(int i = 0; i < _N_CASES_; i++) {
+        for(int j = 0; j < _N_PAIRS_; j++) {
+            grLambda[i][j] = new TGraphErrors(_N_EPSILON_,xValues,lambdy[i][j],xErr,lambdyErr[i][j]);
+            grR[i][j] = new TGraphErrors(_N_EPSILON_,xValues,r[i][j],xErr,rErr[i][j]);
+
+            grLambda[i][j]->SetTitle(Form("%s%s: lambda",cases[i].Data(),pairs[j].Data()));
+            grR[i][j]->SetTitle(Form("%s%s: r",cases[i].Data(),pairs[j].Data()));
+
+            fileOut2->cd();
+            grLambda[i][j]->Write();
+            grR[i][j]->Write();
+        }
+
+    }
+
+    fileOut2->Close();
+    fileOut2->Save();
+
+
+
+
 }
 
 
-void getCorrFunc() {
-    //readLustre();
-    //makeCorrFunc();
-    //makeCorrFuncPairs();
 
-
-}
